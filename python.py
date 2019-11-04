6b7a818faf8afd8f101cfde1b6ff003729fb2f55
Muhammad Hashim <m.hashimemon@gmail.com>
2019-11-05 01:18:30 +0500

    changes commit

--- python.py
+++ python.py
@@ -1,19 +1,19 @@
-# Write a Python program to print the following string in a specific format (see the output).
+import sys
+from datetime import datetime
+from math import pi
 
+
+# Write a Python program to print the following string in a specific format (see the output).
 poem = "Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are"
 print(poem)
 
 
 # Write a Python program to get the Python version you are using
-
-import sys
 pythonVersion = str(sys.version_info[0])+'.'+str(sys.version_info[1]);
 print("You are using python : "+pythonVersion+" version");
 
 
 # Write a Python program to display the current date and time.
-
-from datetime import datetime
 # datetime object containing current date and time
 now = datetime.now()
 # dd/mm/YY H:M:S
@@ -23,8 +23,6 @@ print("date and time =", dt_string)
 
 
 # Write a Python program which accepts the radius of a circle from the user and compute the area.
-
-from math import pi
 r = float(input ("Input the radius of the circle : "))
 print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
 
